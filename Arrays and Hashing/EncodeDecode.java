e3f4c617b53422ba21ede1d2b3d37dad7f7b66bf
Bhanu Rakshita Paul <88148227+bhanuRakshita@users.noreply.github.com>
2025-05-20 15:58:41 -0400

    encode decode setup

---
+++ Arrays and Hashing/EncodeDecode.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeDecode {
+    public String encode(List<String> strs){
+        return "";
+    }
+
+    public List<String> decode(String code){
+        return new ArrayList<>();
+    }
+}
