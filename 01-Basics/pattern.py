3c6c2211c6fa8134dac02de0a727d781dff1fb98
Bhanu Rakshita Paul <88148227+bhanuRakshita@users.noreply.github.com>
2025-06-11 20:54:51 -0400

    Basics-patterns1

---
+++ 01-Basics/pattern.py
@@ -0,0 +1,71 @@
+#   n=3
+#    *
+#   ***
+#  *****
+#  *****
+#   ***
+#    *
+def pattern1(n):
+    for i in range(n):
+        spaces=n-i
+        stars=2*i+1
+        print((spaces*' ')+(stars*'*'))
+    for i in range(n):
+        spaces=i+1
+        stars=2*n-2*i-1
+        print((spaces*' ')+(stars*'*'))
+
+# for N=5
+# 1
+# 01
+# 101
+# 0101
+# 10101
+def pattern2(n):
+    b=1
+    for i in range(n):
+        binary=1 if b==1 else 0
+        for j in range(i+1):
+            print(binary, end="")
+            binary=0 if binary == 1 else 1
+        print()
+        b=0 if b == 1 else 1
+
+def pattern3(n):
+    for i in range(1, n+1):
+        #print first set of numbers
+        spaces=2*(n-i)
+        for j in range(1, i+1):
+            print(j, end='')
+        # print spaces
+        print(spaces*' ',end='')
+        # print second set of numbers
+        for j in range(i):
+            print(i-j, end='')
+        print()
+
+        
+def pattern4(n):
+    counter = 1
+    for i in range(1,n+1):
+        for j in range(1,1+i):
+            print(counter, end=' ')
+            counter+=1
+        print()
+
+def pattern5(n):
+    for i in range(1,n+1):
+        letter=64 #ASCII for A is 65
+        space=n-i
+        #print spaces
+        print(' '*space, end='')
+        #print letters -> first set
+        for j in range(i):
+            print(chr(letter+j+1), end='')
+            
+        #print letter -> second set
+        for j in range(1,i):
+            print(chr(letter+i-j), end='')
+        print()
+
+pattern5(4)
