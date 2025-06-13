f276dac143eb6be46802c1431d14552c6261a131
Bhanu Rakshita Paul <88148227+bhanuRakshita@users.noreply.github.com>
2025-06-13 17:33:34 -0400

    Added pattern prolems

--- 01-Basics/pattern.py
+++ 01-Basics/pattern.py
@@ -31,6 +31,13 @@ def pattern2(n):
         print()
         b=0 if b == 1 else 1
 
+# n=6
+# 1          1
+# 12        21
+# 12       321
+# 1234    4321
+# 12345  54321
+# 123456654321
 def pattern3(n):
     for i in range(1, n+1):
         #print first set of numbers
@@ -44,7 +51,12 @@ def pattern3(n):
             print(i-j, end='')
         print()
 
-        
+# n=5
+# 1
+# 2  3
+# 4  5  6
+# 7  8  9  10
+# 11  12  13  14  15
 def pattern4(n):
     counter = 1
     for i in range(1,n+1):
@@ -52,6 +64,13 @@ def pattern4(n):
             print(counter, end=' ')
             counter+=1
         print()
+
+# n=5
+#      A     
+#     ABA    
+#    ABCBA   
+#   ABCDCBA  
+#  ABCDEDCBA 
 
 def pattern5(n):
     for i in range(1,n+1):
@@ -68,4 +87,66 @@ def pattern5(n):
             print(chr(letter+i-j), end='')
         print()
 
-pattern5(4)
+# n=3
+# ******
+# **  **
+# *    *
+# *    *
+# **  **
+# ******
+def pattern6(n):
+    for i in range(n):
+        stars=n-i
+        spaces=2*i
+        print(stars*'*'+spaces*' '+stars*'*')
+    for i in range(n):
+        stars=i+1
+        spaces=2*(n-i-1)
+        print(stars*'*'+spaces*' '+stars*'*')
+
+# n=3
+# *    *
+# **  **
+# ******
+# **  **
+# *    *
+def pattern7(n):
+    for i in range(n):
+        stars=i+1
+        spaces= 2*(n-i-1)
+        print(stars*'*'+spaces*' '+stars*'*')
+    for i in range(1,n):
+        stars=n-i
+        spaces=2*i
+        print(stars*'*'+spaces*' '+stars*'*')
+
+# n=6
+# ******
+# *    *
+# *    *
+# *    *
+# *    *
+# ******
+def pattern8(n):
+    for i in range(1,n+1):
+        if(i==n or i==1):
+            print('*'*n)
+        else:
+            print('*'+' '*(n-2)+'*')
+
+# n=4
+# 4 4 4 4 4 4 4
+# 4 3 3 3 3 3 4
+# 4 3 2 2 2 3 4
+# 4 3 2 1 2 3 4
+# 4 3 2 2 2 3 4
+# 4 3 3 3 3 3 4
+# 4 4 4 4 4 4 4
+def pattern9(n):
+    for i in range(1, 2*n):
+        for j in range(1, 2*n):
+            num = max(n-min(i,j)+1, max(i,j)-n+1)
+            print(num, end=' ')
+        print()
+
+pattern9(7)
